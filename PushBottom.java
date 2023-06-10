6ed5f1494d71435c9190043161707d62ca6267a5
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-10 18:05:21 +0530

    Add files via upload

---
+++ PushBottom.java
@@ -0,0 +1,25 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class PushBottom {
+    public static void PUSH(Stack<Integer>s,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        PUSH(s, data);
+        s.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer>s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        PUSH(s,4);
+        while(!s.isEmpty()){
+             System.out.println(s.pop());
+        }
+    }
+}
